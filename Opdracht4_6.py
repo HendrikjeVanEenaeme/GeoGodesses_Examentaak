63f5bb592972af31f6ce861a3581ec99f0563020
larakaekebeke <lara.livianne.kaekebeke@vub.be>
2023-12-18 14:33:34 +0100

    van tif naar jpg

--- Opdracht4_6.py
+++ Opdracht4_6.py
@@ -6,13 +6,22 @@ Created on Mon Dec 18 13:29:37 2023
 """
 
 import os
-import datetime
-import calendar
-import collections
-import subprocess
-import csv
 from osgeo import gdal, osr
-from dateutil.relativedelta import relativedelta
 import matplotlib.pyplot as plt
 import numpy as np
+import glob
+
+
+for filename in glob.glob('*.tif'):
+    with open(os.path.join(os.getcwd(), filename), 'r') as f:
+        basename = filename.split(".tif") [0]
+        dataset = gdal.Open(filename)
+        band1 = dataset.GetRasterBand(1)
+        b1 = band1.ReadAsArray()
+        img = np.dstack((b1, ))
+        q = plt.figure()
+        plt.imshow(img)
+        plt.savefig(basename+'.jpg', dpi=600)
+
+
 
