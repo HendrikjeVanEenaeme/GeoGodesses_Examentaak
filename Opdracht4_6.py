6306fbba6ca9a31e821d300ab2748a469e4957e4
larakaekebeke <lara.livianne.kaekebeke@vub.be>
2023-12-18 13:32:39 +0100

    importmodules

--- Opdracht4_6.py
+++ Opdracht4_6.py
@@ -5,3 +5,14 @@ Created on Mon Dec 18 13:29:37 2023
 @author: hendr
 """
 
+import os
+import datetime
+import calendar
+import collections
+import subprocess
+import csv
+from osgeo import gdal, osr
+from dateutil.relativedelta import relativedelta
+import matplotlib.pyplot as plt
+import numpy as np
+
