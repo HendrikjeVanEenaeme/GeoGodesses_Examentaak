6754349c2a026ce645c94ec7bdec59194f08cd84
larakaekebeke <lara.livianne.kaekebeke@vub.be>
2023-12-28 10:13:54 +0100

    code weg

--- Opdracht4_6.py
+++ Opdracht4_6.py
@@ -5,23 +5,6 @@ Created on Mon Dec 18 13:29:37 2023
 @author: hendr
 """
 
-import os
-from osgeo import gdal, osr
-import matplotlib.pyplot as plt
-import numpy as np
-import glob
-
-
-for filename in glob.glob('*.tif'):
-    with open(os.path.join(os.getcwd(), filename), 'r') as f:
-        basename = filename.split(".tif") [0]
-        dataset = gdal.Open(filename)
-        band1 = dataset.GetRasterBand(1)
-        b1 = band1.ReadAsArray()
-        img = np.dstack((b1, ))
-        q = plt.figure()
-        plt.imshow(img)
-        plt.savefig(basename+'.jpg', dpi=600)
 
 
 import os
