dcf680d6b31e6b40cc90cbd02b948674326728f9
HendrikjeVanEenaeme <hendrikje.marie.n.van.eenaeme@vub.be>
2024-01-04 13:43:52 +0100

    ex1 poging 2

--- Ex1.py
+++ Ex1.py
@@ -5,3 +5,16 @@ Created on Thu Jan  4 13:28:33 2024
 @author: hendr
 """
 
+import GIS_functions as gis
+import matplotlib.pyplot as plt
+
+for land_cover_image in gis.list_files_in_folder("DATA/Land_Cover_images"):
+    land_cover_data_array = gis.open_as_array(land_cover_image)
+
+    out_fih = land_cover_image.replace("DATA", "OUT").replace(".tif", ".jpeg")
+
+    plt.figure()
+    plt.imshow(land_cover_data_array)
+    plt.savefig(out_fih, dpi=600)
+    
+    
