5b25731bfa8de8a1972bbdd82d150b496073a7e3
HendrikjeVanEenaeme <hendrikje.marie.n.van.eenaeme@vub.be>
2024-01-04 16:23:23 +0100

    Ex3

--- Ex3.py
+++ Ex3.py
@@ -5,3 +5,16 @@ Created on Thu Jan  4 16:00:51 2024
 @author: hendr
 """
 
+import GIS_functions as gis
+import matplotlib.pyplot as plt
+
+from Ex2 import area_df
+
+locations = area_df.index.droplevel(level="Land cover type").unique()
+
+for location in locations:
+    relative_area_land_cover_type = area_df[area_df.index.isin([location], level="Location")]["% of total area location"]
+    land_cover_types = relative_area_land_cover_type.index.droplevel(level="Location").unique()
+    plt.figure()
+    plt.pie(relative_area_land_cover_type, labels=land_cover_types)
+    plt.savefig(f"OUT/Pie_charts/{location}")
