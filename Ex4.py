882185a06e66dd9ed2c109a5d69b1f8941a748ab
larakaekebeke <lara.livianne.kaekebeke@vub.be>
2024-01-05 18:12:18 +0100

    ex 4 en 5

--- Ex4.py
+++ Ex4.py
@@ -8,7 +8,7 @@ Created on Thu Jan  4 16:00:51 2024
 import GIS_functions as gis
 
 
-# create a function to read the bands of a TIFF-file and calculate NDVI
+# create a function to read the bands of a TIF-file and calculate NDVI
 def calc_NDVI(fih):
     RED_band = gis.open_as_array(fih, bandnumber=1)
     NIR_band = gis.open_as_array(fih, bandnumber=2)
@@ -18,7 +18,7 @@ def calc_NDVI(fih):
     return NDVI
 
 
-# create a function to read the bands of a TIFF-file and calculate SAVI
+# create a function to read the bands of a TIF-file and calculate SAVI
 def calc_SAVI(fih):
     RED_band = gis.open_as_array(fih, bandnumber=1)
     NIR_band = gis.open_as_array(fih, bandnumber=2)
--- Ex5.py
+++ Ex5.py
@@ -5,3 +5,52 @@ Created on Thu Jan  4 16:00:52 2024
 @author: hendr
 """
 
+import GIS_functions as gis
+import matplotlib.pyplot as plt
+
+from Ex4 import calc_NDVI, calc_SAVI
+
+
+for optical_image in gis.list_files_in_folder("DATA/Optical_images"): #insert file path to optical images folder
+    filename = optical_image.split('/')[-1]
+    location = filename.removesuffix(".tif").split('_')[-1].removeprefix("T")
+
+    driver, ndv, xsize, ysize, geot, projection = gis.get_geoinfo(optical_image)
+
+#calling the functions of ex4
+    NDVI = calc_NDVI(optical_image) 
+    SAVI = calc_SAVI(optical_image)
+
+
+    gis.create_geotiff(f"OUT/Optical_images/{location}/NDVI/{filename}",
+                       array=NDVI,
+                       driver=driver,
+                       ndv=ndv,
+                       xsize=xsize,
+                       ysize=ysize,
+                       geot=geot,
+                       projection=projection)
+
+    gis.create_geotiff(f"OUT/Optical_images/{location}/SAVI/{filename}",
+                       array=SAVI,
+                       driver=driver,
+                       ndv=ndv,
+                       xsize=xsize,
+                       ysize=ysize,
+                       geot=geot,
+                       projection=projection)
+    
+    plt.figure()
+    plt.imshow(NDVI, cmap='YlGn', vmin=0, vmax=100)
+    plt.title(f"NDVI Map - {filename}")
+    plt.colorbar(label="NDVI Value")
+    plt.savefig(f"OUT/Optical_images/{location}/JPEG_MAPS/{filename}.jpeg", dpi = 600)
+    
+    plt.figure()
+    plt.imshow(SAVI, cmap='YlGn', vmin=0, vmax=100)
+    plt.title(f"SAVI Map - {filename}")
+    plt.colorbar(label="SAVI Value")
+    plt.savefig(f"OUT/Optical_images/{location}/JPEG_MAPS/{filename}.jpeg", dpi = 600)
+    
+    
+  
