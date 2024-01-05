5cebaf0db874f775436d42f0ffec93b9f2e6ac54
larakaekebeke <lara.livianne.kaekebeke@vub.be>
2024-01-05 15:00:42 +0100

    update ex4

--- Ex4.py
+++ Ex4.py
@@ -5,3 +5,24 @@ Created on Thu Jan  4 16:00:51 2024
 @author: hendr
 """
 
+import GIS_functions as gis
+
+
+# create a function to read the bands of a TIFF-file and calculate NDVI
+def calc_NDVI(fih):
+    RED_band = gis.open_as_array(fih, bandnumber=1)
+    NIR_band = gis.open_as_array(fih, bandnumber=2)
+
+    NDVI = (NIR_band - RED_band) / (NIR_band + RED_band)
+
+    return NDVI
+
+
+# create a function to read the bands of a TIFF-file and calculate SAVI
+def calc_SAVI(fih):
+    RED_band = gis.open_as_array(fih, bandnumber=1)
+    NIR_band = gis.open_as_array(fih, bandnumber=2)
+
+    SAVI = 1.5 * (NIR_band - RED_band) / (NIR_band + RED_band + 0.5)
+
+    return SAVI
