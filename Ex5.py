e9ccb236f99f6dd3102841d061d28a710235eaea
larakaekebeke <lara.livianne.kaekebeke@vub.be>
2024-01-05 19:12:45 +0100

    ex 5 output

--- Ex5.py
+++ Ex5.py
@@ -11,8 +11,8 @@ import GIS_functions as gis
 from Ex4 import calc_NDVI, calc_SAVI
 
 
-for optical_image in gis.list_files_in_folder("DATA/Optical_images"): #insert file path to optical images folder
-    filename = optical_image.split('/')[-1]
+for optical_image in gis.list_files_in_folder("DATA\\Optical_images"): #insert file path to optical images folder
+    filename = optical_image.split('\\')[-1]
     location = filename.removesuffix(".tif").split('_')[-1].removeprefix("T")
 
     driver, ndv, xsize, ysize, geot, projection = gis.get_geoinfo(optical_image)
@@ -22,7 +22,7 @@ for optical_image in gis.list_files_in_folder("DATA/Optical_images"): #insert fi
     SAVI = calc_SAVI(optical_image)
 
 
-    gis.create_geotiff(f"OUT/Optical_images/{location}/NDVI/{filename}",
+    gis.create_geotiff(f"OUT\\Optical_images\\{location}\\NDVI\\{filename}",
                        array=NDVI,
                        driver=driver,
                        ndv=ndv,
@@ -31,7 +31,7 @@ for optical_image in gis.list_files_in_folder("DATA/Optical_images"): #insert fi
                        geot=geot,
                        projection=projection)
 
-    gis.create_geotiff(f"OUT/Optical_images/{location}/SAVI/{filename}",
+    gis.create_geotiff(f"OUT\\Optical_images\\{location}\\SAVI\\{filename}",
                        array=SAVI,
                        driver=driver,
                        ndv=ndv,
