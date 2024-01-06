cba0cadd59803ae41f25c091f3f95b650b05745d
larakaekebeke <lara.livianne.kaekebeke@vub.be>
2024-01-06 11:41:46 +0100

    ex 5 output.2

--- Ex5.py
+++ Ex5.py
@@ -6,22 +6,25 @@ Created on Thu Jan  4 16:00:52 2024
 """
 
 import GIS_functions as gis
+import matplotlib.pyplot as plt
 
 
 from Ex4 import calc_NDVI, calc_SAVI
 
-
-for optical_image in gis.list_files_in_folder("DATA\\Optical_images"): #insert file path to optical images folder
+#create a loop for all files in optical images folder
+#create variables filename and location to structure the output
+for optical_image in gis.list_files_in_folder("DATA\\Optical_images"): 
     filename = optical_image.split('\\')[-1]
     location = filename.removesuffix(".tif").split('_')[-1].removeprefix("T")
 
+#calling metadata of the source files
     driver, ndv, xsize, ysize, geot, projection = gis.get_geoinfo(optical_image)
 
 #calling the functions of ex4
     NDVI = calc_NDVI(optical_image) 
     SAVI = calc_SAVI(optical_image)
 
-
+#export the output as TIF-files
     gis.create_geotiff(f"OUT\\Optical_images\\{location}\\NDVI\\{filename}",
                        array=NDVI,
                        driver=driver,
@@ -40,6 +43,21 @@ for optical_image in gis.list_files_in_folder("DATA\\Optical_images"): #insert f
                        geot=geot,
                        projection=projection)
     
+#create a NDVI and SAVI map per optical image 
+    mapname = filename.removesuffix(".tif")
+
+    plt.figure()
+    plt.imshow(NDVI, cmap='YlGn', vmin=0, vmax=1)
+    plt.title(f"NDVI Map - {mapname}")
+    plt.colorbar(label="NDVI Value")
+    plt.savefig(f"OUT\\Optical_images\\{location}\\JPEG_MAPS\\NDVI\\{mapname}.jpeg", dpi = 600)
+
+    plt.figure()
+    plt.imshow(SAVI, cmap='YlGn', vmin=0, vmax=1)
+    plt.title(f"SAVI Map - {mapname}")
+    plt.colorbar(label="SAVI Value")
+    plt.savefig(f"OUT\\Optical_images\\{location}\\JPEG_MAPS\\SAVI\\{mapname}.jpeg", dpi = 600)
+
 
     
     
