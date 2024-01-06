c05db693f6df93c494f4c3c9239a454db85062eb
monajcontipelli <mona.jillian.contipelli@vub.be>
2024-01-06 17:08:38 +0100

    ex 2 en 3

--- Ex2.py
+++ Ex2.py
@@ -9,26 +9,33 @@ import GIS_functions as gis
 import numpy as np
 import pandas as pd
 
+#Create an empty Pandas dataframe to store information in python
 area_df = pd.DataFrame()
 
+#To loop through each file in the DATA/Land_Cover_images folder, get geospatial info on the current land cover image and extract location information from the filename by splitting it up
 for land_cover_image in gis.list_files_in_folder("DATA/Land_Cover_images"):
     geo_info = gis.get_geoinfo(land_cover_image)
     location = land_cover_image.split('/')[-1].removesuffix('.tif').split('_')[-1]
-
+    
+    #calculate the total number of pixels
     total_number_pixels = geo_info[2] * geo_info[3]
+    
     pixel_values = gis.open_as_array(land_cover_image)
 
+    #To count pixel values (=land cover type) and how often they occure 
     land_cover_types, pixel_counts = np.unique(pixel_values, return_counts=True)
-
-    absolute_area_land_cover_type = pixel_counts * 100
-    relative_area_land_cover_type = pixel_counts / total_number_pixels
+    #To calculate the absolute and relative area of each land cover type in square kilometres ans as a percentage of the total area
+    absolute_area_land_cover_type = pixel_counts / 10000
+    relative_area_land_cover_type = (pixel_counts / total_number_pixels) *100
     
+    #Creating Pandas Dataframe to store the results for the current image 
     df = pd.DataFrame({"Location": location,
                        "Land cover type": land_cover_types,
-                       "Area [m^2]": absolute_area_land_cover_type,
+                       "Area [km^2]": absolute_area_land_cover_type,
                        "% of total area location": relative_area_land_cover_type})
-
+    #Adding new dataframe to already existing dataframe
     area_df = pd.concat([area_df, df])
-    
+
+#Setting a multi-level index using the columns 'Location' and 'Land cover type'
 area_df = area_df.set_index(keys=["Location", "Land cover type"])
 
--- Ex3.py
+++ Ex3.py
@@ -5,3 +5,25 @@ Created on Thu Jan  4 16:00:51 2024
 @author: hendr
 """
 
+import GIS_functions as gis
+import matplotlib.pyplot as plt 
+
+from Ex2 import area_df
+
+# Extract unique locations from the index of the area_df DataFrame by dropping land cover type
+locations = area_df.index.droplevel(level="Land cover type").unique()
+
+#Loop through each location
+for location in locations:
+    # Filter the DataFrame for the current location and extract the relative area of land cover types
+    relative_area_land_cover_type = area_df[area_df.index.isin([location], level="Location")]["% of total area location"]
+    # Extract unique land cover types for the current location
+    land_cover_types = relative_area_land_cover_type.index.droplevel(level="Location").unique()
+
+#Create a pie chart with title and legend that shows the relative area of land cover type and save as jpeg
+    plt.figure(figsize=(8, 8)) 
+    plt.pie(relative_area_land_cover_type, labels=land_cover_types)
+    plt.title(f"Land Cover Distribution in {location}")
+    plt.legend(land_cover_types, title="Land Cover Types", bbox_to_anchor=(1, 1), loc="upper left")
+    plt.savefig(f"OUT/Pie_charts/{location}", format = 'jpeg') #To save the figure as a jpeg
+    
