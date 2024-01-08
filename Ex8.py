7cf596ce12faf6942ae1596cd1b66b65c03adbb1
Basten Dekoninck <basten.dekoninck@riskconcile.com>
2024-01-08 21:23:59 +0100

    Converted the multi-index table to xlsx and csv

--- Ex8.py
+++ Ex8.py
@@ -4,4 +4,9 @@ Created on Thu Jan  4 16:03:46 2024
 
 @author: hendr
 """
+import pandas as pd
 
+from Ex7 import area_df
+
+area_df.to_excel("OUT/area_table.xlsx")
+area_df.to_csv("OUT/area_table.csv")
