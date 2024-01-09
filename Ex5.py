5ff42ad918d72be4ed6ac4ceec48fc1800da5ed1
larakaekebeke <lara.livianne.kaekebeke@vub.be>
2024-01-09 20:49:56 +0100

    ex5

--- Ex5.py
+++ Ex5.py
@@ -18,7 +18,6 @@ from Ex4 import calc_NDVI, calc_SAVI
 #create variables filename and location to structure the output
 for optical_image in gis.list_files_in_folder(os.path.join("DATA","Optical_images")): 
     filename = os.path.basename(optical_image)
-    print(fil)
     location = filename.removesuffix(".tif").split('_')[-1].removeprefix("T")
 
 #calling metadata of the source files
@@ -52,14 +51,14 @@ for optical_image in gis.list_files_in_folder(os.path.join("DATA","Optical_image
 
     plt.figure()
     plt.imshow(NDVI, cmap='YlGn', vmin=0, vmax=1)
-    plt.title(f"NDVI Map - {mapname}")
+    plt.title(f"NDVI Map - {mapname}", y = 1.05)
     plt.colorbar(label="NDVI Value")
     plt.savefig(f'{os.path.join("OUT", "Optical_images", location, "JPEG_MAPS", "NDVI", mapname)}.jpeg', dpi = 600)
     plt.close()
 
     plt.figure()
     plt.imshow(SAVI, cmap='YlGn', vmin=0, vmax=1)
-    plt.title(f"SAVI Map - {mapname}")
+    plt.title(f"SAVI Map - {mapname}", y = 1.05)
     plt.colorbar(label="SAVI Value")
     plt.savefig(f'{os.path.join("OUT", "Optical_images", location, "JPEG_MAPS", "SAVI", mapname)}.jpeg', dpi = 600)
     plt.close()
