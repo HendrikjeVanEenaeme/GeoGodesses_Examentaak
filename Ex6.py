59a31d1f70c7cee3616d3651a11cb7d88bf781d2
HendrikjeVanEenaeme <hendrikje.marie.n.van.eenaeme@vub.be>
2024-01-10 15:41:04 +0100

    Ex6

--- Ex6.py
+++ Ex6.py
@@ -15,19 +15,15 @@ for location in ["35MPT", "35MQS"]:
     for index_type in ["NDVI", "SAVI"]:
         # We initialize the index_arrays list, where the different arrays of pixel values of that
         # index type for that location (as calculated in Ex. 5) in the time series get stored.
-        # The idea is then to take for each pixel (per location) the mean value of the index 
-        # (NDVI or SAVI) throughout the whole time series
         index_arrays = []
-        for index_map in gis.list_files_in_folder(os.path.join("OUT", "Optical_images", location, index_type)):
-            index_arrays.append(gis.open_as_array(index_map))
-
-        # We calculate the array of pixel averages with np.nanmean. The function np.nanmean
-        # has as effect that whenever a pixel has no index value at some point in time (and hence
-        # has a NaN-value in its respective index array of that timepoint), that particular pixel
-        # for that particular time is disregarded from the calculation of the average.
+        for index_image in gis.list_files_in_folder(os.path.join("OUT", "Optical_images", location, index_type)):
+            index_arrays.append(gis.open_as_array(index_image))
+            
+        # We calculate the array of pixel averages with np.nanmean 
+        # This function disregards pixels with no index value (NaN-values) from the calculation
         index_average_array = np.nanmean(index_arrays, axis=0)
 
-        # We store the so-calculated index average arrays as a tif-file with the same specs
+        # We store the index average arrays as a tif-file with the same specs
         # as the land cover image of the corresponding location, to make sure we can make the
         # correct comparisons in Ex. 7.
         index_average_filename = f'{os.path.join("OUT", "Index_averages", f"{location}_{index_type}_average")}.tif'
@@ -42,3 +38,16 @@ for location in ["35MPT", "35MQS"]:
                        ysize=ysize,
                        geot=geot,
                        projection=projection)
+        
+        
+        #We create an average NDVI and average SAVI map per location 
+        mapname = index_average_filename.removesuffix(".tif")
+        
+        plt.figure()
+        plt.imshow(index_average_array, cmap='YlGn', vmin=0, vmax=1)
+        plt.title(f" Average Map - {mapname}")
+        plt.colorbar(label="Value")
+        plt.savefig(f'{os.path.join("OUT", "index_averages", "JPEG_MAPS_averages", mapname)}.jpeg', dpi = 600)
+        plt.close()
+
+ 
